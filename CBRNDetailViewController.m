03cde055d64838f3f9f3c429d3cc003c6c76ed08
Chris Bowdon <c.bowdon@gmail.com>
2011-11-12 20:10:15 +0000

    naive implementation of detail view

--- CBRNDetailViewController.m
+++ CBRNDetailViewController.m
@@ -15,33 +15,33 @@
 
 -(NSArray*)sections
 {
-	if (_sections != nil) {
-		return _sections;
+	if (_sections == nil) {		
+		NSMutableArray *mut = [[NSMutableArray alloc] init];
+		
+		[mut addObject:[NSString stringWithFormat:@"Half life"]];
+		[mut addObject:[NSString stringWithFormat:@"Atomic number"]];
+		
+		if (self.radioisotope.nProgeny > 0) {
+			[mut addObject:[NSString stringWithFormat:@"Progeny"]];
+		}
+		if (self.radioisotope.nAlphas > 0) {
+			[mut addObject:[NSString stringWithFormat:@"Alphas"]];
+		}
+		if (self.radioisotope.nBetas > 0) {
+			[mut addObject:[NSString stringWithFormat:@"Betas"]];
+		}
+		if (self.radioisotope.nPositrons > 0) {
+			[mut addObject:[NSString stringWithFormat:@"Positrons"]];
+		}
+		if (self.radioisotope.nNegatrons > 0) {
+			[mut addObject:[NSString stringWithFormat:@"Negatrons"]];
+		}
+		if (self.radioisotope.nPhotons > 0) {
+			[mut addObject:[NSString stringWithFormat:@"Photons"]];
+		}
+		
+		_sections = [[NSArray alloc] initWithArray:mut];
 	}
-	NSMutableArray *mut = [[NSMutableArray alloc] init];
-	[mut addObject:[NSString stringWithFormat:@"Radionuclide"]];
-	[mut addObject:[NSString stringWithFormat:@"Mass and atomic number"]];
-	
-	if (self.radioisotope.nProgeny > 0) {
-	[mut addObject:[NSString stringWithFormat:@"Progeny"]];
-	}
-	if (self.radioisotope.nAlphas > 0) {
-	[mut addObject:[NSString stringWithFormat:@"Alphas"]];
-	}
-	if (self.radioisotope.nBetas > 0) {
-	[mut addObject:[NSString stringWithFormat:@"Betas"]];
-	}
-	if (self.radioisotope.nPositrons > 0) {
-	[mut addObject:[NSString stringWithFormat:@"Positrons"]];
-	}
-	if (self.radioisotope.nNegatrons > 0) {
-	[mut addObject:[NSString stringWithFormat:@"Negatrons"]];
-	}
-	if (self.radioisotope.nPhotons > 0) {
-	[mut addObject:[NSString stringWithFormat:@"Photons"]];
-	}
-	
-	_sections = [[NSArray alloc] initWithArray:mut];
 	return _sections;
 }
 
@@ -50,7 +50,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+	
 	self.title = self.radioisotope.name;
 }
 
@@ -69,12 +69,12 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+	return 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"RadioisotopeCell";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
@@ -82,20 +82,42 @@
     }
     
     // Configure the cell...
-    
-	cell.textLabel.text = @"Hello";
 	
+	NSString *sectionName = [self.sections objectAtIndex:indexPath.section];
+	
+	if ([sectionName isEqualToString:@"Atomic number"]) {		
+		cell.textLabel.text = [NSString stringWithFormat:@"%i", self.radioisotope.atomicNumber];		
+	} else if ([sectionName isEqualToString:@"Half life"]){			
+		cell.textLabel.text = [NSString stringWithFormat:@"%.3f %@", [self.radioisotope.halfLifeNumber doubleValue], self.radioisotope.halfLifeUnit];;
+	} else if ([sectionName isEqualToString:@"Progeny"]) {
+		Progeny *prog = [self.radioisotope.progeny objectAtIndex:indexPath.row];
+		NSString *line = [NSString stringWithFormat:@"%.3f%c\t %@", 100*[prog.probability doubleValue], '%', prog.name];	
+		cell.textLabel.text = line;
+	} else if ([sectionName isEqualToString:@"Alphas"]) {
+		DiscreteParticle *alpha = [self.radioisotope.alphas objectAtIndex:indexPath.row];
+		double energy = [alpha.energy doubleValue];
+		double probability = 100*[alpha.probability doubleValue];
+		NSString *line = [NSString stringWithFormat:@"%.3f%c\t %.3f MeV", probability, '%', energy];	
+		cell.textLabel.text = line;
+	} else if ([sectionName isEqualToString:@"Betas"]) {
+		ContinuousParticle *beta = [self.radioisotope.betas objectAtIndex:indexPath.row];
+		double energy = [beta.energy doubleValue];
+		double maxEnergy = [beta.maxEnergy doubleValue];
+		double probability = 100*[beta.probability doubleValue];
+		NSString *line = [NSString stringWithFormat:@"%.3f%c\t %.3f MeV (max)\t [%.3f MeV (avg)]", probability, '%', maxEnergy, energy];	
+		cell.textLabel.text = line;
+	} else {
+		cell.textLabel.text = @"Unknown";
+	}
     return cell;
 }
 
 #pragma mark Section header titles
 
-/*
- HIG note: In this case, since the content of each section is obvious, there's probably no need to provide a title, but the code is useful for illustration.
- */
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section 
 {    
-    return [self.sections objectAtIndex:section];
+    // to do: make this "Alphas (13)"
+	return [self.sections objectAtIndex:section];
 }
 
 
